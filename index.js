818ac4503b704453c2e086efbe506cfff10aa7c7
Deepthakre <118052733+Deepthakre@users.noreply.github.com>
2023-02-28 12:53:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,362 @@
+/* Quiz Source: W3schools.Com */
+var Quiz = {
+    "JS": [
+    {
+    "Id": 1,
+    "Question": "Inside Which HTML Element Do We Put The JavaScript?",
+    "Options": [
+    {
+    "A": "&Lt;Script&Gt;",
+    "B": "&Lt;Javascript&Gt;",
+    "C": "&Lt;Scripting&Gt;",
+    "D": "&Lt;Js&Gt;"
+    }
+    ],
+    "Answer": "&Lt;Script&Gt;",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 2,
+    "Question": "Where Is The Correct Place To Insert A JavaScript?",
+    "Options": [
+    {
+    "A": "The &Lt;Head&Gt; Section",
+    "B": "The &Lt;Body&Gt; Section",
+    "C": "Both The &Lt;Head&Gt; Section And The &Lt;Body&Gt; Section Are Correct"
+    }
+    ],
+    "Answer": "Both The &Lt;Head&Gt; Section And The &Lt;Body&Gt; Section Are Correct",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 3,
+    "Question": "What Is The Correct Syntax For Referring To An External Script Called 'Xxx.Js'?",
+    "Options": [
+    {
+    "A": "&Ltscript Href=&Quot;Xxx.Js&Quot;>",
+    "B": "&Lt;Script Name=&Quot;Xxx.Js&Quot;&Gt;",
+    "C": "&Lt;Script Src=&Quot;Xxx.Js&Quot;&Gt;"
+    }
+    ],
+    "Answer": "&Lt;Script Src=&Quot;Xxx.Js&Quot;&Gt;",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 4,
+    "Question": "The External JavaScript File Must Contain The &Lt;Script&Gt; Tag.",
+    "Options": [
+    {
+    "A": "True",
+    "B": "False"
+    }
+    ],
+    "Answer": "False",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 5,
+    "Question": "How Do You Write &Quot;Hello World&Quot; In An Alert Box?",
+    "Options": [
+    {
+    "A": "AlertBox(&Quot;Hello World&Quot;);",
+    "B": "Msg(&Quot;Hello World&Quot;);",
+    "C": "Alert(&Quot;Hello World&Quot;);",
+    "D": "MsgBox(&Quot;Hello World&Quot;);",
+    }
+    ],
+    "Answer": "Alert(&Quot;Hello World&Quot;);",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 6,
+    "Question": "How Do You Create A Function In JavaScript?",
+    "Options": [
+    {
+    "A": "Function MyFunction()",
+    "B": "Function:myFunction()",
+    "C": "Function = MyFunction()",
+    }
+    ],
+    "Answer": "Function MyFunction()",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 7,
+    "Question": "How Do You Call A Function Named &Quot;MyFunction&Quot;?",
+    "Options": [
+    {
+    "A": "Call Function MyFunction()",
+    "B": "Call MyFunction()",
+    "C": "MyFunction()",
+    }
+    ],
+    "Answer": "MyFunction()",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 8,
+    "Question": "How To Write An IF Statement In JavaScript?",
+    "Options": [
+    {
+    "A": "If I = 5 Then",
+    "B": "If I == 5 Then",
+    "C": "If (I == 5)",
+    "D": " If I = 5",
+    }
+    ],
+    "Answer": "If (I == 5)",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 9,
+    "Question": "Which Of The Following Is A Disadvantage Of Using JavaScript?",
+    "Options": [
+    {
+    "A": "Client-Side JavaScript Does Not Allow The Reading Or Writing Of Files.",
+    "B": "JavaScript Can Not Be Used For Networking Applications Because There Is No Such Support Available.",
+    "C": "JavaScript Doesn't Have Any Multithreading Or Multiprocess Capabilities.",
+    "D": "All Of The Above."
+    }
+    ],
+    "Answer": "All Of The Above.",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 10,
+    "Question": "How To Write An IF Statement For Executing Some Code If &Quot;I&Quot; Is NOT Equal To 5?",
+    "Options": [
+    {
+    "A": "If (I <> 5)",
+    "B": "If I <> 5",
+    "C": "If (I != 5)",
+    "D": "If I =! 5 Then",
+    }
+    ],
+    "Answer": "If (I != 5)",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 11,
+    "Question": "How Does A WHILE Loop Start?",
+    "Options": [
+    {
+    "A": "While I = 1 To 10",
+    "B": "While (I &Lt;= 10; I++)",
+    "C": "While (I &Lt;= 10)"
+    }
+    ],
+    "Answer": "While (I &Lt;= 10)",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 12,
+    "Question": "How Does A FOR Loop Start?",
+    "Options": [
+    {
+    "A": "For (I = 0; I &Lt;= 5)",
+    "B": "For (I = 0; I &Lt;= 5; I++)",
+    "C": "For I = 1 To 5",
+    "D": "For (I &Lt;= 5; I++)"
+    }
+    ],
+    "Answer": "For (I = 0; I &Lt;= 5; I++)",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 13,
+    "Question": "How Can You Add A Comment In A JavaScript?",
+    "Options": [
+    {
+    "A": "//This Is A Comment",
+    "B": "&Sbquo;This Is A Comment",
+    "C": "&Lt;!--This Is A Comment--&Gt;"
+    }
+    ],
+    "Answer": "//This Is A Comment",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 14,
+    "Question": "How To Insert A Comment That Has More Than One Line?",
+    "Options": [
+    {
+    "A": "/*This Comment Has More Than One Line*/",
+    "B": "//This Comment Has More Than One Line//",
+    "C": "&Lt;!--This Comment Has More Than One Line--&Gt;"
+    }
+    ],
+    "Answer": "/*This Comment Has More Than One Line*/",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 15,
+    "Question": "What Is The Correct Way To Write A JavaScript Array?",
+    "Options": [
+    {
+    "A": "Var Colors = (1:&Quot;Red&Quot;, 2:&Quot;Green&Quot;, 3:&Quot;Blue&Quot;)",
+    "B": "Var Colors = [&Quot;Red&Quot;, &Quot;Green&Quot;, &Quot;Blue&Quot;]",
+    "C": "Var Colors = 1 = (&Quot;Red&Quot;), 2 = (&Quot;Green&Quot;), 3 = (&Quot;Blue&Quot;)",
+    "D": "Var Colors = &Quot;Red&Quot;, &Quot;Green&Quot;, &Quot;Blue&Quot;"
+    }
+    ],
+    "Answer": "Var Colors = [&Quot;Red&Quot;, &Quot;Green&Quot;, &Quot;Blue&Quot;]",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 16,
+    "Question": "How Do You Round The Number 7.25, To The Nearest Integer?",
+    "Options": [
+    {
+    "A": "Rnd(7.25)",
+    "B": "Math.Round(7.25)",
+    "C": "Math.Rnd(7.25)",
+    "D": "Round(7.25)"
+    }
+    ],
+    "Answer": "Math.Round(7.25)",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 17,
+    "Question": "How Do You Find The Number With The Highest Value Of X And Y?",
+    "Options": [
+    {
+    "A": "Math.Max(X, Y)",
+    "B": "Math.Ceil(X, Y)",
+    "C": "Top(X, Y)",
+    "D": "Ceil(X, Y)"
+    }
+    ],
+    "Answer": "Math.Max(X, Y)",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 18,
+    "Question": "What Is The Correct JavaScript Syntax For Opening A New Window Called &Quot;W2&Quot;?",
+    "Options": [
+    {
+    "A": "W2 = Window.New(&Quot;Http://Www.W3schools.Com&Quot;);",
+    "B": "W2 = Window.Open(&Quot;Http://Www.W3schools.Com&Quot;);"
+    }
+    ],
+    "Answer": "W2 = Window.Open(&Quot;Http://Www.W3schools.Com&Quot;);",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 19,
+    "Question": "JavaScript Is The Same As Java.",
+    "Options": [
+    {
+    "A": "True",
+    "B": "False"
+    }
+    ],
+    "Answer": "False",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 20,
+    "Question": "How Can You Detect The Client&Rsquo;S Browser Name?",
+    "Options": [
+    {
+    "A": "Navigator.AppName",
+    "B": "Browser.Name",
+    "C": "Client.NavName"
+    }
+    ],
+    "Answer": "Navigator.AppName",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 21,
+    "Question": "Which Event Occurs When The User Clicks On An HTML Element?",
+    "Options": [
+    {
+    "A": "Onchange",
+    "B": "Onclick",
+    "C": "Onmouseclick",
+    "D": "Onmouseover"
+    }
+    ],
+    "Answer": "Onclick",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 22,
+    "Question": "How Do You Declare A JavaScript Variable?",
+    "Options": [
+    {
+    "A": "Var CarName;",
+    "B": "Variable CarName;",
+    "C": "V CarName;"
+    }
+    ],
+    "Answer": "Var CarName;",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 23,
+    "Question": "Which Operator Is Used To Assign A Value To A Variable?",
+    "Options": [
+    {
+    "A": "*",
+    "B": "-",
+    "C": "=",
+    "D": "X"
+    }
+    ],
+    "Answer": "=",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 24,
+    "Question": "What Will The Following Code Return: Boolean(10 &Gt; 9)",
+    "Options": [
+    {
+    "A": "NaN",
+    "B": "False",
+    "C": "True"
+    }
+    ],
+    "Answer": "True",
+    "Score": 0,
+    "Status": ""
+    },
+    {
+    "Id": 25,
+    "Question": "Is JavaScript Case-Sensitive?",
+    "Options": [
+    {
+    "A": "No",
+    "B": "Yes"
+    }
+    ],
+    "Answer": "Yes",
+    "Score": 0,
+    "Status": ""
+    }
+    ]
+    }
